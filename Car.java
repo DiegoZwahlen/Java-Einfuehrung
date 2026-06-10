c6584ce926b2fffdee77dd223dd649fcd1600211
Diego Zwahlen <Diego.Zwahlen@baloise.ch>
2026-06-10 10:55:54 +0200

    Finished Car.java Script

--- Car.java
+++ Car.java
@@ -1,5 +1,29 @@
 public class Car {
-    public static void main(String[] args) {
+    String brand;
+    String model;
+    int buildYear;
 
+    // Konstruktor: setzt alle Attribute
+    public Car(String brand, String model, int buildYear) {
+        this.brand = brand;
+        this.model = model;
+        this.buildYear = buildYear;
+    }
+
+    public void PrintCarInfo() {
+        System.out.println("Marke: " + brand);
+        System.out.println("Modell: " + model);
+        System.out.println("Baujahr: " + buildYear);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Car auto1 = new Car("BMW", "M3", 2020);
+        Car auto2 = new Car("Ferrari", "F40", 1990);
+        Car auto3 = new Car("Peel", "P50", 1962);
+
+        auto1.PrintCarInfo();
+        auto2.PrintCarInfo();
+        auto3.PrintCarInfo();
     }
 }
