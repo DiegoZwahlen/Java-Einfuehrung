40ada40ccbfa66ca0df44bfc422cbd486268ca11
Diego Zwahlen <Diego.Zwahlen@baloise.ch>
2026-06-16 07:45:15 +0200

    Added Getter and Setter Methods to Car.java as well as toString method

--- Car.java
+++ Car.java
@@ -1,20 +1,43 @@
 public class Car {
-    String brand;
-    String model;
-    int buildYear;
+    private String brand;
+    private String model;
+    private int buildYear;
 
-    // Konstruktor: setzt alle Attribute
     public Car(String brand, String model, int buildYear) {
         this.brand = brand;
         this.model = model;
         this.buildYear = buildYear;
     }
 
-    public void PrintCarInfo() {
-        System.out.println("Marke: " + brand);
-        System.out.println("Modell: " + model);
-        System.out.println("Baujahr: " + buildYear);
-        System.out.println();
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBuildYear(int buildYear) {
+        this.buildYear = buildYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Marke: " + brand + "\n"
+             + "Modell: " + model + "\n"
+             + "Baujahr: " + buildYear;
     }
 
     public static void main(String[] args) {
@@ -22,8 +45,12 @@ public class Car {
         Car auto2 = new Car("Ferrari", "F40", 1990);
         Car auto3 = new Car("Peel", "P50", 1962);
 
-        auto1.PrintCarInfo();
-        auto2.PrintCarInfo();
-        auto3.PrintCarInfo();
+        System.out.println(auto1);
+        System.out.println();
+
+        System.out.println(auto2);
+        System.out.println();
+
+        System.out.println(auto3);
     }
 }
